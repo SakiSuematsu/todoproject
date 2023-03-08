a901e42d430908150c1e7e5ca2fb4fecffe8030a
SakiSuematsu <h2522a2742@gmail.com>
2023-03-08 15:26:21 +0900

    fourth

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -122,6 +122,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
